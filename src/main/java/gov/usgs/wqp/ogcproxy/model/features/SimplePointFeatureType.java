ed0c418a2dc55279a31c729a241a69819536d3cc
Mary Bucknell <mbucknell@usgs.gov>
2017-03-28 15:30:05 -0500

    Tabs rather than spaces

--- src/main/java/gov/usgs/wqp/ogcproxy/model/features/SimplePointFeatureType.java
+++ src/main/java/gov/usgs/wqp/ogcproxy/model/features/SimplePointFeatureType.java
@@ -9,32 +9,33 @@ import gov.usgs.wqp.ogcproxy.model.attributes.FeatureAttributeType;
 
 public class SimplePointFeatureType {
 
-	public static final SimpleFeatureType FEATURETYPE;
-	static {
-		SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
-		builder.setName("Location");
-		// Coordinate reference system
-		builder.setSRS("EPSG:4326");
+    public static final SimpleFeatureType FEATURETYPE;
 
-		// add attributes in order
-		builder.add("the_geom", Point.class);
-		builder.length(32).add(FeatureAttributeType.orgId.toString(), String.class);
-		builder.length(128).add(FeatureAttributeType.orgName.toString(), String.class);
-		builder.length(32).add(FeatureAttributeType.name.toString(), String.class);
-		builder.length(256).add(FeatureAttributeType.locName.toString(), String.class);
-		builder.length(32).add(FeatureAttributeType.type.toString(), String.class);
-		builder.length(32).add(FeatureAttributeType.searchType.toString(), String.class);
-		builder.length(32).add(FeatureAttributeType.huc8.toString(), String.class);
-		builder.length(32).add(FeatureAttributeType.provider.toString(), String.class);
-		builder.length(32).add(FeatureAttributeType.sampleCnt.toString(), String.class);
-		builder.length(32).add(FeatureAttributeType.resultCnt.toString(), String.class);
-                builder.length(32).add(FeatureAttributeType.activityCn.toString(), String.class);
+    static {
+        SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
+        builder.setName("Location");
+        // Coordinate reference system
+        builder.setSRS("EPSG:4326");
 
-		// build the type
-		FEATURETYPE = builder.buildFeatureType();
-	}
+        // add attributes in order
+        builder.add("the_geom", Point.class);
+        builder.length(32).add(FeatureAttributeType.orgId.toString(), String.class);
+        builder.length(128).add(FeatureAttributeType.orgName.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.name.toString(), String.class);
+        builder.length(256).add(FeatureAttributeType.locName.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.type.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.searchType.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.huc8.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.provider.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.sampleCnt.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.resultCnt.toString(), String.class);
+        builder.length(32).add(FeatureAttributeType.activityCn.toString(), String.class);
 
-	private SimplePointFeatureType() {
-	}
+        // build the type
+        FEATURETYPE = builder.buildFeatureType();
+    }
+
+    private SimplePointFeatureType() {
+    }
 
 }
